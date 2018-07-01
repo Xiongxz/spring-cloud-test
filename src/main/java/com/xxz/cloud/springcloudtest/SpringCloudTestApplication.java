3132222c7510059d81c2c817eb392a3832844f81
Xiongxz <654962327@qq.com>
2018-07-01 15:31:07 +0800

    ss

--- src/main/java/com/xxz/cloud/springcloudtest/SpringCloudTestApplication.java
+++ src/main/java/com/xxz/cloud/springcloudtest/SpringCloudTestApplication.java
@@ -2,11 +2,15 @@ package com.xxz.cloud.springcloudtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer//启动一个服务注册中心提供给其他应用进行对话
 public class SpringCloudTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudTestApplication.class, args);
+        //SpringApplication.run(SpringCloudTestApplication.class, args);
+        new SpringApplicationBuilder(SpringCloudTestApplication.class).web(true).run(args);
     }
 }
